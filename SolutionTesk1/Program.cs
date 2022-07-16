7a85a6452948492147a80ee303852d6ef25c8eec
MusaSagov <sagov82@mail.ru>
2022-07-17 00:32:01 +0300

    add Solutio Tesk1

---
+++ SolutionTesk1/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null){
+   int inputNumberOne = int.Parse(inputLkneOne);
+   int inputNumberTwo = int.Parse(inputLkneTwo);
+
+   if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+   {
+     Console.WriteLine("yes");
+   } 
+  else
+  {
+    Console.WriteLine("no");
+  }
+
+}
