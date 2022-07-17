476827fe56bb27fc43c4772db9bf91aacda3df63
MusaSagov <sagov82@mail.ru>
2022-07-18 01:55:42 +0300

    add Solution Tesk3

---
+++ SolutionTesk3/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLkneOne = Console.ReadLine();
+
+if(inputLkneOne != null)
+{
+int inputNumber = int.Parse(inputLkneOne);
+
+int startNumber = (inputNumber * -1);
+
+string lineOutput = "";
+while (startNumber < inputNumber)
+{
+    lineOutput = lineOutput + startNumber +", ";
+    startNumber++;
+}
+
+lineOutput = lineOutput + inputNumber;
+Console.WriteLine(lineOutput);
+}
