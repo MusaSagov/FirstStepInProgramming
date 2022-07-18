b047300d485abc40dbfc3232f2879c1aed02a5bd
MusaSagov <sagov82@mail.ru>
2022-07-19 01:25:15 +0300

    add Home Work S.Tesk1

---
+++ HomeWorkSolutionTesk1/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+
+    if(inputNumberOne > inputNumberTwo)
+    {
+        Console.WriteLine($"max = {inputNumberOne}");
+        Console.WriteLine($"min = {inputNumberTwo}");
+    }
+    else{
+        Console.WriteLine($"max = {inputNumberTwo}");
+        Console.WriteLine($"min = {inputNumberOne}");
+    }
+}
