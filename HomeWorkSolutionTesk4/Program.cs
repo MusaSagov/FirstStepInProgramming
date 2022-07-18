97a2e819f6c040fcde37d5ee847b6a27eee6e77d
MusaSagov <sagov82@mail.ru>
2022-07-19 02:35:14 +0300

    add Home Work Solution Tesk4

---
+++ HomeWorkSolutionTesk4/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{   
+    int inputNumber = int.Parse(inputLine);
+        
+
+    int startNumber = 2;
+
+    string lineOutput = "";
+
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = lineOutput + startNumber+", ";
+        startNumber = startNumber + 2;
+    }
+    lineOutput = lineOutput + startNumber;
+    Console.WriteLine(lineOutput);
+  
+    
+}
