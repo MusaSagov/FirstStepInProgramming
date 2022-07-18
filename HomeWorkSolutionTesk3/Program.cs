d4942ba2105a1ee628f134c7f6bd762ec71a0d5c
MusaSagov <sagov82@mail.ru>
2022-07-19 02:23:00 +0300

    add Home WorkSolution Tesk3

---
+++ HomeWorkSolutionTesk3/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputLkwLine = Console.ReadLine();
+
+if(inputLkwLine != null)
+{
+    int inputLine = int.Parse(inputLkwLine);
+    
+    if(inputLine%2 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
