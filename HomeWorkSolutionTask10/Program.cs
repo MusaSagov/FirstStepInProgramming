16b3e46d404873cca2f2c740c1afb8fc8aec4f1a
MusaSagov <sagov82@mail.ru>
2022-07-23 02:05:08 +0300

    Home Work SolutionTask10

---
+++ HomeWorkSolutionTask10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)//Трехзначное число
+{
+    int inputNumberOne = int.Parse(inputLine);//превращаем строк. перем.- в целочисленное
+    int inputNumberTwo = (inputNumberOne % 100)/10;//остаток от деления "456"/100=56,
+    // деление 56/10= 5
+    
+    Console.WriteLine(inputNumberTwo);
+}    
