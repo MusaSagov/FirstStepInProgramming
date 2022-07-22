6d12661c25bc469426257bb56d4978c12366ae77
MusaSagov <sagov82@mail.ru>
2022-07-23 01:16:37 +0300

    add solution Task12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,18 @@
+﻿//Решение Николая
+string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null)
+{
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+    int div =inputNumberTwo%inputNumberOne;
+    if(div ==0){
+        Console.WriteLine("yes");
+    }
+    else{
+        Console.WriteLine(div);
+    }
+}
+
+//Htit
