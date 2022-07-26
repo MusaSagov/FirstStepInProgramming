3422134ef018873ba32bfd41dd57d13a0b262022
MusaSagov <sagov82@mail.ru>
2022-07-27 01:36:19 +0300

    Solution Task 20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,49 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+int coordXPointA; //вводим переменные если убираем код в метод
+int coordYPointA; 
+int coordXPointB;           //Глобальные переменные всегда объявляются сверху методов
+int coordYPointB;
+double lengthAB;
+
+//Метод считывания координат точек А и В
+void readDataofPoint()
+{
+    Console.WriteLine("Введите координату Х в точке А");
+    coordXPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y в точке А");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х в точке B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y в точке B");
+    coordYPointB = int.Parse(Console.ReadLine());
+}
+
+//Метод вычисляет расстояние между точками А и В
+void conculatelengthAB()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
+}
+
+readDataofPoint();
+conculatelengthAB();
+
+
+// Console.WriteLine("Введите координату Х в точке А"); //ввод координат без метода
+// int coordXPointA = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y в точке А");
+// int coordYPointA = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Х в точке B");
+// int coordXPointB = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y в точке B");
+// int coordYPointB = int.Parse(Console.ReadLine());
+
+// double lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2)); //Math.Sqrt - корень квадратный Math.Pow - возведение в степень double - введение чисел с десятичными дробями  //расчет длины между точками без метода
+
+Console.WriteLine(lengthAB);
