b80cd5289957ee0966e072321dc5969f435a3f4b
MusaSagov <sagov82@mail.ru>
2022-07-29 00:06:00 +0300

    HomeWorkSolutionTask23

---
+++ HomeWorkSolutionTask23/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+
+    string line1 = "";
+    string line2 = string.Empty; //Пустая строка
+    int s = 1;
+
+    while (s <= numberN)
+    {
+        line1 = line1 + s + " ";
+        line2 = line2 + (s * s * s) + " ";
+        s++;
+    }
+
+    Console.WriteLine(line1);
+    Console.WriteLine(line2);
+}
