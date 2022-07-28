471448e133e3037043892380a3b7ea6d5ea5e72e
MusaSagov <sagov82@mail.ru>
2022-07-29 00:00:32 +0300

    HomeWorkSolutionTask21

---
+++ HomeWorkSolutionTask21/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//вводим переменные если убираем код в метод
+//Глобальные переменные всегда объявляются сверху методов
+int coordXPointA; 
+int coordYPointA;
+int coordZPointA;  
+int coordXPointB;          
+int coordYPointB;
+int coordZPointB;
+double lengthAB;
+
+//Метод считывания координат точек А и В
+void readDataofPoint()
+{
+    Console.WriteLine("Введите координату Х в точке А");
+    coordXPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y в точке А");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z в точке А");
+    coordZPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х в точке B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y в точке B");
+    coordYPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z в точке B");
+    coordZPointB = int.Parse(Console.ReadLine());
+}
+
+//Метод вычисляет расстояние между точками А и В в 3D пространстве
+void conculatelengthAB()
+{
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointB - coordXPointA), 2) + Math.Pow((coordYPointB - coordYPointA), 2) + Math.Pow((coordZPointB - coordZPointA), 2));
+}
+
+readDataofPoint();
+conculatelengthAB();
+
