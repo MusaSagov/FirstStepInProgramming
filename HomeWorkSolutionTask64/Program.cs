ff30bbb5c7be9864bb4ce17d9a6f40781629f0fa
MusaSagov <sagov82@mail.ru>
2022-08-27 20:57:12 +0300

    add HomeWorkSolutionTask64

---
+++ HomeWorkSolutionTask64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+int numberM = ReadData("Задайте число M"); // использование метода превращения чисел в вещественные
+int numberN = ReadData("Задайте число N");
+
+if (numberM < numberN) // определяем какое из введенных чисел больше, какое меньше
+{
+    NaturalNumberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNumberPrinter(numberN, numberM);
+}
+
+
+int ReadData(string line) // метод превращения введенных чисел в вещественные
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int sum = 0;
+
+void NaturalNumberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " "); // печать всех чисел в диапазоне
+    NaturalNumberPrinter(m + 1, n); // определение диапазона
+}
